afb1ea65f62e4cbfcb3d91bddf5ac24999f7bfc8
GokulA2006 <yokeshgokul8@gmail.com>
2026-08-25 20:23:59 +0530

    Add files via upload

---
+++ decrement.js
@@ -0,0 +1,15 @@
+//decrement operator
+var a=10
+a--
+console.log(a)
+
+//decrement operator 1
+var b=10
+var c = a--
+console.log(c)
+console.log(b)
+
+//pre decrement operator
+var e=10
+var d =--e
+console.log(d)
